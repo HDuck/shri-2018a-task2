ac9cd2233df608cc5a4f41382fe5964543c2c94c
HDuck <HardcoreDuck@users.noreply.github.com>
2018-08-05 22:16:39 +0300

    Добавлена заглушка в desktop версии

---
+++ src/js/dummy.js
@@ -0,0 +1,45 @@
+"use strict";
+
+//Функция для расчета параметра top для прижатия заглушки к низу
+function dummyLoc() {
+  var DUMMY_BOTTOM = 493;
+  var FOOT_BOTTOM = 759;
+  
+  var dummy = document.getElementById('dummy');
+  
+  var dumOffsParT = dummy.offsetTop;
+  var dumOffsWinB = dummy.getBoundingClientRect().bottom;
+  var footOffsWinB = document.getElementsByClassName('footer')[0].getBoundingClientRect().bottom;
+
+  var dumEndLocT = dumOffsParT + (DUMMY_BOTTOM - dumOffsWinB) - (FOOT_BOTTOM - footOffsWinB);
+
+  return dumEndLocT + 'px';
+}
+
+function dummyDisappear(evt) {
+  
+  evt.stopPropagation();
+  
+  //Добавляем анимацию исчезновения заглушки
+  var dummy = document.getElementById('dummy');
+  dummy.className = 'devices__dummy_hide';
+  
+  return true;
+}
+
+function dummyAppear() {
+  
+  var dummy = document.getElementById('dummy');
+  var dummyTop = dummyLoc();  //Получаем top
+  
+  //Показываем заглушку, если она далеко от конца прокрутки
+  if (parseInt(dummyTop) < 420) {
+    dummy.style.top = dummyTop;
+    dummy.className = 'devices__dummy_show';
+    return true;
+    
+  } else {
+    
+    return false;
+  }
+}
--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,19 @@
+"use strict";
+
 function init() {
   
+  //Добавляем обработчик для всех device__item на клик
   var devs = document.getElementsByClassName('devices__item');
   for (var i = 0; i < devs.length; i++) {
     devs[i].addEventListener('click', openPopUp);
   }
+
+  //Добавляем обработчик для анимации заглушки в desktop версии
+  var devList = document.getElementsByClassName('devices')[0];
+  devList.addEventListener('touchstart', dummyDisappear);
+  devList.addEventListener('touchend', function() {
+    setTimeout(dummyAppear, 400);  //Задержка, чтобы учесть прокрутку после конца прикосновения
+  });
 }
 
 window.onload = init;
